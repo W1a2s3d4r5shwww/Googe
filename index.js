0b3cde8c31dedef551cf0fe1c1743eac8eb0c320
Fai <shinniunndoes@gmail.com>
2025-10-28 15:11:30 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+import dotenv from "dotenv";
+import server from "./Server.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Proxy server started on port ${PORT}`);
+});
